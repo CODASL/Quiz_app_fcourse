daee5ef9ac46278ecb76e6425de8f9761abff6c5
MukiBwoi <muksith20000309@gmail.com>
2022-01-30 18:17:08 +0530

    responsive changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quiz_app/Constants/colors.dart';
@@ -6,13 +7,16 @@ import 'package:quiz_app/Providers/providers.dart';
 import 'package:quiz_app/Routers/route_names.dart';
 import 'Routers/route.dart';
 
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: providers,
